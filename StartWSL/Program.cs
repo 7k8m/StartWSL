a3f059d6c411f5b74e46296771d2697c46cedf28
Tomohito Nakayama <tmht@7k8m.me>
2022-06-18 21:00:45 +0900

    Pass command from input

--- StartWSL/Program.cs
+++ StartWSL/Program.cs
@@ -1,3 +1,12 @@
 ﻿using System.Diagnostics;
 
-Process.Start("wsl.exe").WaitForExit();
+var p = Process.Start(new ProcessStartInfo() { FileName = "wsl.exe", RedirectStandardInput = true});
+
+var si = p.StandardInput;
+si.NewLine = "\n";
+
+String? line = null;
+while ( (line = Console.ReadLine() ) != null )
+{
+    si.WriteLine(line);
+}
